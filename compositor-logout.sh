c3d14c764aab3cbab9fcded08f3bf1ed0b5426e9
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-02-23 20:43:48 +0100

    Compositor logout

--- compositor-logout.sh
+++ compositor-logout.sh
@@ -6,15 +6,14 @@ Hyprland)
   hyprshutdown --vt "$VT"
   ;;
 Niri)
-  systemd-run --no-block sh -c "sleep 1 && chvt $VT"
+  systemd-run --no-block sh -c "sleep 3 && uwsm stop; sleep 1 && chvt $VT"
   niri msg action quit
   ;;
 Sway)
-  systemd-run --no-block sh -c "sleep 1 && chvt $VT"
+  systemd-run --no-block sh -c "sleep 3 && uwsm stop; sleep 1 && chvt $VT"
   swaymsg exit
   ;;
 *)
-  systemd-run --no-block sh -c "sleep 1 && chvt $VT"
-  uwsm stop
+  systemd-run --no-block sh -c "sleep 3 && uwsm stop; sleep 1 && chvt $VT"
   ;;
 esac
