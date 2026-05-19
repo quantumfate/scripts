23494bb1c07fa383ae6c28f58168d39512260e5d
Leon Connor Holm <l.c.holm@proton.me>
2026-05-19 17:40:51 +0200

    fix: removed wrapper

--- wrapper/minimize.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-class=$(hyprctl activewindow -j | jq -r ".class")
-
-if [ "$class" = "steam" ]; then
-  xdotool getactivewindow windowunmap
-else
-  hyprctl dispatch "hl.dsp.window.kill(activewindow)"
-fi
